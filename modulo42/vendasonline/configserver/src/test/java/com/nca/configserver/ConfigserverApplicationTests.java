361f35ef51c8d153dcb126f63b7b45f285e000e3
alison <adevpr11@gmail.com>
2024-11-22 17:44:12 -0300

    Commit inicial do exercício do módulo42

---
+++ modulo42/vendasonline/configserver/src/test/java/com/nca/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nca.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ modulo42/vendasonline/vendaservice/src/main/java/com/nca/vendaservice/VendaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nca.vendaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaserviceApplication.class, args);
+	}
+
+}
